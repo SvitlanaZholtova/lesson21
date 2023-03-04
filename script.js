fdf77adff0031ad75c31974930d9dc22e69923e7
SvitlanaZholtova <ryzhaja88@gmail.com>
2023-03-04 18:11:58 +0200

    Min and max found

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const arr = [
     852,
     234,
     'apple',
-    13,
+    -13,
     'cat',
     862,
     741,
     852,
     'dog',
-    963,
+    -963,
     321,
     'bread',
     789
@@ -24,6 +24,25 @@ for(let i = 0; i < arr.length; i++) {
     }
   }
 
-
 console.log(sum);
 
+
+/*let minNum = Infinity; 
+let maxNum = -Infinity; */
+
+let minNum = arr[0];
+let maxNum = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === 'number') {
+    if (arr[i] < minNum) {
+      minNum = arr[i];
+    }
+    if (arr[i] > maxNum) {
+      maxNum = arr[i];
+    }
+  }
+}
+
+console.log(`Мінімальне число: ${minNum}`); 
+console.log(`Максимальне число: ${maxNum}`); 
