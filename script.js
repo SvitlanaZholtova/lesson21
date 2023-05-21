e74a209e37a7c7735f7d8824411e097b5000b04c
SvitlanaZholtova <ryzhaja88@gmail.com>
2023-05-21 16:57:06 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,57 +1,44 @@
-const arr = [
-    14,
-    852,
-    234,
-    'apple',
-    -13,
-    'cat',
-    862,
-    741,
-    852,
-    'dog',
-    -963,
-    321,
-    'bread',
-    789
-];
 
 
-let sum = 0;
+let a = prompt ("Enter first number");
+let b = prompt ("Enter second number");
 
-for(let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === 'number') { 
-      sum += arr[i];
-    }
-  }
-
-console.log(sum);
-
-
-/*let minNum = Infinity; 
-let maxNum = -Infinity; */
-
-let minNum = arr[0];
-let maxNum = arr[0];
-
-for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] === 'number') {
-    if (arr[i] < minNum) {
-      minNum = arr[i];
-    }
-    if (arr[i] > maxNum) {
-      maxNum = arr[i];
-    }
-  }
+function sum(a, b) {
+  if (typeof a === Number && typeof b === Number) {
+  return a + b;
 }
-
-console.log(`Мінімальне число: ${minNum}`); 
-console.log(`Максимальне число: ${maxNum}`); 
-
-let output = '';
-for (let a = 1; a <= 5; a++) {
-  for (let b = 1; b <= a; b++) {
-    output += '#';
-  }
-  output += '\n';
+  else
+  {return "Error! Please, enter numbers";
 }
-console.log(output);
+};
+console.log (sum);
+
+function subtraction(a, b) {
+  if (typeof a === Number && typeof b === Number) {
+    return a - b;
+  }
+    else
+    {return "Error! Please, enter numbers";
+  }
+};
+console.log (subtraction (a, b));
+
+function division(a, b) {
+  if (typeof a === Number && typeof b === Number && b!==0) {
+    return a / b;
+  }
+    else
+    {return "Error";
+  }
+};
+console.log (division (a, b));
+
+function multiplication(a, b) {
+  if (typeof a === Number && typeof b === Number) {
+    return a * b;
+  }
+    else
+    {return "Error";
+  }
+};
+console.log (multiplication (a, b));
